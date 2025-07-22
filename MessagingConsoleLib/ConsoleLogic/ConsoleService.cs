73522e49cb3b27c11e355909cb2b253630a38e3f
zacdev5 <zackorck@gmail.com>
2025-07-22 15:35:49 +0200

    create ConsoleService and extract interface IConsoleService

---
+++ MessagingConsoleLib/ConsoleLogic/ConsoleService.cs
@@ -0,0 +1,23 @@
+﻿namespace MessagingConsoleLib.ConsoleLogic;
+
+public class ConsoleService : IConsoleService
+{
+    public Task<string> PromptForNameAsync()
+    {
+        Console.Write("Enter Name: ");
+        string? input = Console.ReadLine();
+
+        while (string.IsNullOrWhiteSpace(input))
+        {
+            Console.Write("Name cannot be empty. Please enter a name: ");
+            input = Console.ReadLine();
+        }
+
+        return Task.FromResult(input);
+    }
+
+    public void WriteMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
---
+++ MessagingConsoleLib/ConsoleLogic/IConsoleService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MessagingConsoleLib.ConsoleLogic
+{
+    public interface IConsoleService
+    {
+        Task<string> PromptForNameAsync();
+        void WriteMessage(string message);
+    }
+}
